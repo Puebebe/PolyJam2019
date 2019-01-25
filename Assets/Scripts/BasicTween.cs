7cfada7382a8a0a9369d1eb7001f651140777db0
arma <arma@netcompany.com>
2019-01-25 22:55:01 +0100

    Dotween plugin + test scene.

---
+++ Assets/Scripts/BasicTween.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTween : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MyCallback();
+    }
+
+    private void MyCallback()
+    {
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(transform.DOMoveX(5, 1));
+        mySequence.Append(transform.DOMoveX(0, 1));
+        mySequence.OnComplete(MyCallback);
+    }
+        
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
