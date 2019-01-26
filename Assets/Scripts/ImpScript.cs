c80cfebd8a907113febe29f3792d4cd97372fc54
sasqoc <artur.madela@gmail.com>
2019-01-26 23:10:34 +0100

    Imp upgrader

---
+++ Assets/Scripts/ImpScript.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpScript : MonoBehaviour {
+
+    public Animator anim;
+    public Transform[] targetsList;
+
+    public void StartUpgrade(float number)
+    {
+       Vector3 targ = targetsList[(int)number].position;
+       Sequence mySequence = DOTween.Sequence();
+       mySequence.Append(transform.DOMove(new Vector3(targ.x - 15, targ.y, 0), 0f));
+       mySequence.Append(transform.DOMove(new Vector3(targ.x, targ.y, 0), 2f));
+       mySequence.AppendCallback(AttackAnim);
+       mySequence.AppendInterval(0.5f);
+       mySequence.Append(transform.DOMove(new Vector3(targ.x + 15, targ.y, 0), 2f));
+    }
+
+    void AttackAnim()
+    {
+        anim.SetTrigger("AttackTrig");
+    }
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour {
+
+    public ImpScript imp;
+
+	// Use this for initialization
+	void Start () {
+        imp.StartUpgrade(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
