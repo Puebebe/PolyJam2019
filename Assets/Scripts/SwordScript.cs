155b5e2ca9cba86ea2ffb9579c2884912ddfdb0a
sasqoc <artur.madela@gmail.com>
2019-01-26 14:04:04 +0100

    Sword dmg.

--- Assets/Scripts/SwordScript.cs
+++ Assets/Scripts/SwordScript.cs
@@ -6,10 +6,22 @@ public class SwordScript : MonoBehaviour
 {
 
     [SerializeField] float maxlifeTime = 1.5f;
+    public int damage = 10;
 
     private void Start()
-    {       
+    {
         Destroy(this.gameObject, maxlifeTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            col.gameObject.GetComponent<snailControler>().ApplyDamage(damage);
+
+            Destroy(this.gameObject, maxlifeTime);
+        }
     }
 
     void Update()
