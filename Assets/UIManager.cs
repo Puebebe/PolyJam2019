7281a8f238296112e270591ffb4efd632a009f95
Dirtowladny <39705586+Dirtowladny@users.noreply.github.com>
2019-01-26 01:29:22 +0100

    UIManager HealthBarsImplemented
    added health bars and a test button

---
+++ Assets/UIManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField]
+    private Image PlayerHP;
+    private RectTransform PlayerHPRT;
+    [SerializeField]
+    private Image EnemyHP;
+    private RectTransform EnemyHPRT;
+
+    private List<int> PlayerHPList = new List<int>();
+    private List<int> EnemyHPList = new List<int>();
+
+    private int PlayerHPMaxSize;
+    private int EnemyHPMaxSize;
+
+    private void Awake()
+    {
+        if (PlayerHP == null)
+        {
+            PlayerHP = FindImageOfName("PlayerHealthBar");
+        }
+        if (EnemyHP == null)
+        {
+            PlayerHP = FindImageOfName("EnemyHealthBar");
+        }
+
+        PlayerHPRT = PlayerHP.GetComponent<RectTransform>();
+        EnemyHPRT = EnemyHP.GetComponent<RectTransform>();
+
+        PlayerHPMaxSize = Mathf.FloorToInt(PlayerHPRT.sizeDelta.x);
+
+        EnemyHPMaxSize = Mathf.FloorToInt(EnemyHPRT.sizeDelta.x);
+    }
+
+    private Image FindImageOfName(string imageName)
+    {
+        var possibleObjects = GameObject.FindObjectsOfType<Image>();
+        for (int i = 0; i < possibleObjects.Length; i++)
+        {
+            if (possibleObjects[i].name == imageName)
+            {
+                return possibleObjects[i];
+            }
+        }
+        return null;
+    }
+
+    public void UpdatePlayerHP(int hp)
+    {
+        PlayerHPList.Add(hp);
+        if (PlayerHPList.Count == 1)
+        {
+            InitalizePlayerHP();
+        }
+        else
+        {
+            StartCoroutine(animateImage(PlayerHPRT, PlayerHPMaxSize, hp, PlayerHPList[PlayerHPList.Count - 2], PlayerHPList[0]));
+        }
+    }
+
+    public void UpdateEnemyHP(int hp)
+    {
+        EnemyHPList.Add(hp);
+        if (EnemyHPList.Count == 1)
+        {
+            InitalizeEnemyHP();
+        }
+        else
+        {
+            StartCoroutine(animateImage(EnemyHPRT, PlayerHPMaxSize, hp, PlayerHPList[PlayerHPList.Count - 2], PlayerHPList[0]));
+        }
+    }
+
+    private void InitalizeEnemyHP()
+    {
+        Debug.Log("Inicjalizacja EnemyHP");
+    }
+
+    private void InitalizePlayerHP()
+    {
+        //obecnie gówno trzeba robić w inicjalizacji lul
+        Debug.Log("inicjalizacja PlayerHP");
+    }
+
+    IEnumerator animateImage(RectTransform img, int maxSize, int newHP, int oldHP, int maxHP)
+    {
+        Debug.Log("animuje");
+        Debug.Log("MaxSize: " + maxSize + " newHP: " + newHP + " oldHP: " + oldHP + " maxHP: " + maxHP);
+        //Debug.Log("SizeDelta: " + img.sizeDelta.x);
+        var rect = img.sizeDelta;
+        for (int i = 0; i < 2; i++)
+        {
+            rect.x = (float)maxSize * ((float)newHP / (float)maxHP);
+            img.sizeDelta = rect;
+            yield return new WaitForSecondsRealtime(0.1f);
+            rect.x = (float)maxSize * ((float)oldHP / (float)maxHP);
+            img.sizeDelta = rect;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+        rect.x =(float)maxSize * ((float)newHP / (float)maxHP);
+        img.sizeDelta = rect;
+
+    }
+
+    public void ForceUpdateUI()
+    {
+
+    }
+
+    int x = 0;
+
+    public void Test()
+    {
+        UpdatePlayerHP(100 - x);
+        UpdateEnemyHP(100 - x);
+        x += 20;
+    }
+    
+}
