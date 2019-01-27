06d7e98671a37f0bf24e0abe85c8cbfc917a2861
sasqoc <artur.madela@gmail.com>
2019-01-27 12:44:13 +0100

    Golem added.

---
+++ Assets/Scripts/GolemScript.cs
@@ -0,0 +1,48 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemScript : MonoBehaviour {
+    [SerializeField] float attackTimmer = 2;
+    [SerializeField] Transform atackSpawnPos;
+
+    [SerializeField] Animator anim;
+
+    [SerializeField] GameObject attack1Prefab;
+
+    private float delay = 0;
+    private float atackDelay = 0.25f;
+    private void Start()
+    {
+        delay = attackTimmer;
+    }
+
+    void Update()
+    {
+        if (delay < 0)
+        {
+            StartAttack();
+        }
+        else
+        {
+            delay -= Time.deltaTime;
+        }
+    }
+
+    public void StartAttack()
+    {
+        anim.SetTrigger("AttackTrig");
+        float rndDelay = Random.Range(0, 1);
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.AppendInterval(0.25f);
+        mySequence.OnComplete(SpawnWave);
+        delay = attackTimmer + rndDelay;
+    }
+
+    public void SpawnWave()
+    {
+        GameObject Created = Instantiate(attack1Prefab, transform.position, attack1Prefab.transform.rotation);
+        //Created.transform.position = Vector3.zero;
+    }
+}
