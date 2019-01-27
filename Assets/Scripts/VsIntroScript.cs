6cf4a28f8ca40f984708e4c12279d3bd30938db5
sasqoc <artur.madela@gmail.com>
2019-01-27 13:43:54 +0100

    Vs golem.

--- Assets/Scripts/VsIntroScript.cs
+++ Assets/Scripts/VsIntroScript.cs
@@ -18,14 +18,14 @@ public class VsIntroScript : MonoBehaviour {
         blackMask.GetComponent<SpriteRenderer>().material.DOFade(1.0f, 1.2f);
 
         Sequence mySequence = DOTween.Sequence();
-        mySequence.AppendInterval(1.5f);
+        mySequence.AppendInterval(2.4f);
         mySequence.OnComplete(Finish);
 
         Sequence snailSequence = DOTween.Sequence();
-        snailSequence.Append(snail.transform.DOMove(snailTarget.position, 1.0f)).SetEase(Ease.InOutCubic);
+        snailSequence.Append(snail.transform.DOMove(snailTarget.position, 1.3f)).SetEase(Ease.InOutCubic);
 
         Sequence enemySequence = DOTween.Sequence();
-        enemySequence.Append(enemy.transform.DOMove(enemyTarget.position, 1.0f)).SetEase(Ease.InOutCubic);
+        enemySequence.Append(enemy.transform.DOMove(enemyTarget.position, 1.3f)).SetEase(Ease.InOutCubic);
         //transform.DO
     }
 	
