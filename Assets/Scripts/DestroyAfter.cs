45dd3fd615712398fa911901453295686500ce27
Dirtowladny <39705586+Dirtowladny@users.noreply.github.com>
2019-01-27 14:18:14 +0100

    gamepad tutorial

---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour {
+
+    public float delay;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, delay);
+	}
+	
+	
+}
