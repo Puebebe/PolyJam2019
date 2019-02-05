30c081b62c262ad2e8a7262299f56fd7b7c50c9f
Unknown <kawaxbart@gmail.com>
2019-02-05 12:31:44 +0100

    Add skip tutorial

---
+++ Assets/Scripts/SkipTutorial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipTutorial : MonoBehaviour
+{
+	void Update ()
+    {
+        float leftX, rightX;
+
+        if (Input.GetJoystickNames().Length > 0 && (Input.GetJoystickNames()[0] == "Controller (XBOX 360 For Windows)" || Input.GetJoystickNames()[0] == "Controller (Xbox 360 Wireless Receiver for Windows)"))
+        {
+            leftX = Input.GetAxis("HorizontalJoystickL");
+            rightX = Input.GetAxis("HorizontalJoystickR");
+        }
+        else
+        {
+            leftX = Mathf.Sign(Input.GetAxis("HorizontalL"));
+            rightX = Mathf.Sign(Input.GetAxis("HorizontalR"));
+        }
+
+        if (leftX == -1 && rightX == -1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
